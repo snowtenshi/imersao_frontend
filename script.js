596c48cd22058666122c01d3d7afc5f6246ef63c
Gabriela Mariano <gabrielamariano@live.com>
2024-01-28 12:15:03 -0300

    Algumas mudanças no json e no js

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
-    fetch(url)
-        .then((response) => response.json())
-        .then((result) => displayResults(result))
+    const url = `http://127.0.0.1:3000/artists?name_like=${searchTerm}`
+    fetch(url, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then((response) => response.json())
+    .then((result) => displayResults(result))
+    .catch((error) => console.error('Error fetching data:', error))
 }
 
 function displayResults(result) {
